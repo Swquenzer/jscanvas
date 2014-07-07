945899a0be55fea46fe2dac5a369f42254a17c6e
Stephen Quenzer <swquenzer@gmail.com>
2014-07-07 15:26:53 -0400

    Remove extraneous files -Add initial movement of dots away from cursor

--- 1/js/main.js
+++ 1/js/main.js
@@ -1,14 +1,9 @@
 //Paperscript
-
-function onResize(event) {
-	//c1.position = view.center;
-	console.log("IN");
-}
-
 //Constants
 var SPACING = 30;
 var X_CENTER = view.viewSize.width/2;
 var Y_CENTER = view.viewSize.height/2;
+var NUM_ROWS = 30;
 
 //Create Grid of dots
 var grid = [];
@@ -24,3 +19,16 @@ var grid = [];
 		}
 	}
 })(30);
+
+function onMouseMove(event) {
+	for(var i=0; i<NUM_ROWS; i++) {
+		for(var j=0; j<NUM_ROWS; j++) {
+			var xDist = event.point.x - grid[i][j].position.x;
+			var yDist = event.point.y - grid[i][j].position.y;
+			if((Math.abs(xDist) < 5) && (Math.abs(yDist) < 5)) {
+				if(Math.abs(xDist) <5) grid[i][j].position -= xDist;
+				if(Math.abs(yDist) <5) grid[i][j].position -= yDist;
+ 			}
+		}
+	}
+}
