cc65c03df132c0d64082cbd02c4f251d594a3d6a
Stephen Quenzer <swquenzer@gmail.com>
2014-07-08 12:06:13 -0400

    Update Colors & functionality

--- 1/js/main.js
+++ 1/js/main.js
@@ -1,10 +1,12 @@
 //Paperscript
 //Constants
-var SPACING = 30;
+var SPACING = 20;
 var X_CENTER = view.viewSize.width/2;
 var Y_CENTER = view.viewSize.height/2;
-var NUM_ROWS = 30;
+var NUM_ROWS = 25;
 var PUSH_DIST = 20 ;
+var DOT_RADIUS = 10;
+var isBeingDragged = false;
 
 //Create Grid of dots
 var grid = [];
@@ -14,13 +16,13 @@ var grid = [];
 		for(var j=0; j<numRows; j++) {
 			grid[i][j] = new Shape.Circle({
 				center: [i*SPACING, j*SPACING],
-			    radius: 10,
+			    radius: DOT_RADIUS,
 			    strokeColor: 'black',
 			    fillColor: '9dd5ea'
 			});
 		}
 	}
-})(30);
+})(NUM_ROWS);
 
 var magnet = new Shape.Circle({
 	center: view.center,
@@ -42,24 +44,34 @@ function onMouseMove(event) {
 			if((Math.abs(xDist) < PUSH_DIST) && (Math.abs(yDist) < PUSH_DIST)) {
 				//change color of dot
 				dot.strokeColor = "green";
-				dot.fillColor = "#a4ea9d";
+				console.log(dot.fillColor.toCSS(true));
+				if(dot.fillColor.toCSS(true) == "#9dd5ea") {
+					dot.fillColor = "a4ea9d";
+				} else {
+					dot.fillColor = "e2ea9d";
+				}
 				dot.blendMode = "overlay";
 				//and then check for distance and direction to move dot
 				if(Math.abs(xDist) < PUSH_DIST) dot.position.x -= xDist/2;
 				if(Math.abs(yDist) < PUSH_DIST) dot.position.y -= yDist/2;
+				//finally, check if the pushed dot is now in the goal
+				if(magnet.contains(dot.position)) {
+					dot.fillColor = "red";
+				}
 			}
 		}
 	}
 }
 
+
+/*
 function onFrame(event) {
 	for(var i=0; i<NUM_ROWS; i++) {
 		for(var j=0; j<NUM_ROWS; j++) {
 			//For every dot in the grid...
 			var dot = grid[i][j];
-			if(magnet.contains(dot.position)) {
-			dot.fillColor = "red";
-			}
+			
 		}
 	}
 }
+*/
