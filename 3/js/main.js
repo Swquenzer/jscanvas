c275a82e4efb222a1a9fbeab4fb2757cf28615c7
Stephen Quenzer <swquenzer@gmail.com>
2014-07-23 14:10:13 -0400

    Make ball class include its own physics rules

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,39 +1,54 @@
-//Paperscript
+//.034 meters/pixel
+//17 meters total (view size)
 var FPS = 60;
 var gravity = 9.81; //m/s/s
 
-function ball(x, y, vx, vy, radius, color) {
-	this.shape = new Shape.Circle({
-		center: [x,y],
-		radius: radius,
-		fillColor: color
-	});
-	this.vx = vx*FPS;
-	this.vy = vy*FPS;
+function simulate() {
+	function ball(x, y, vx, vy, radius, color) {
+		vx *= FPS;
+		vy *= FPS;
+
+		this.shape = new Shape.Circle({
+			center: [x,y],
+			radius: radius,
+			fillColor: color
+		});
+
+		this.shape.onFrame = function(event) {
+			//Floor
+			if(this.position.y+this.radius >= view.size.height && vy > 0) {
+				vy *= -1;
+			}
+			//Walls
+			if(this.position.x+this.radius >= view.size.width && vx < view.size.width
+				|| this.position.x-this.radius < 0 && vx < 0) {
+				vx *= -1;
+			}
+
+			//Ball Physics
+			vy += gravity;
+			this.position.x += vx/FPS;
+			this.position.y += vy/FPS;
+		}
+	}
+
+	//b1 = new ball(20, 7, 0, 0, 15,'blue');
+	//b2 = new ball(20, 7, 3, 0, 15, 'red');
+	b3 = new ball(20, 200, 4, -6, 15, 'green');
+
+	var counter = document.getElementById("counter");
+	function onFrame(event) {
+		//Do something every second
+		if(event.count%60 === 0) {
+			counter.innerHTML = event;
+		}
+	}
 }
 
-b = new ball(50,20,5,0,15,'blue');
-
-var counter = document.getElementById("counter");
-function onFrame(event) {
-	//Do something every second
-	if(event.count%60 === 0) {
-		counter.innerHTML = event;
-	}
-	//MAIN CODE
-	//Floor
-	if(b.shape.position.y-b.shape.radius/2 === view.size.y) {
-		b.shape.position
-	}
-
-	//Ball Physics
-	b.vy += gravity;
-	b.shape.position.x += b.vx/FPS;
-	b.shape.position.y += b.vy/FPS;
-
+var startInt = null;
+window.start = function() {
+	startInt = setInterval(function() {simulate()}, 200);
 }
-
-
-window.button = function() {
-	console.log("Clicked");
+window.stop = function() {
+	clearInterval(startInt);
 }
